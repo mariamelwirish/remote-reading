806ec2ac73992d94e9b40028bf32086e0ee8d467
mariamelwirish <mariamelwirish@gmail.com>
2026-04-15 23:02:28 -0400

    cleaned the repo for redesign

--- Server Code/php/get-infants.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once('connect.php');
-    
-    $query = "SELECT * FROM `infants`";
-    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-    $rows = mysqli_num_rows($result);
-    
-?>
